e03e34c942359bfbf5858e04a2d5f31a34255e03
Navya Sri Alaparthi <alaparthins@outlook.com>
2022-10-22 21:36:03 -0500

    updated QRScanner Activity

--- app/src/main/java/com/example/sampleapplication/QRScannerActivity.java
+++ app/src/main/java/com/example/sampleapplication/QRScannerActivity.java
@@ -5,3 +5,13 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+public class QRScannerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qrscanner);
+
+
+    }
+}

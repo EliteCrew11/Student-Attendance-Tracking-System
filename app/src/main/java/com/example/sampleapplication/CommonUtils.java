22b9c7795def92d40f4669ef01b4d234576c3ac6
saikumarm007 <saikumarmudhagoni82@gmail.com>
2022-10-22 20:16:44 -0500

    Imported Libraries and given connection

--- app/src/main/java/com/example/sampleapplication/CommonUtils.java
+++ app/src/main/java/com/example/sampleapplication/CommonUtils.java
@@ -1,3 +1,10 @@
+package com.example.sampleapplication;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
 public class CommonUtils {
-    
-}
+
+    public static boolean isConnectedToInternet(Context context){
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);

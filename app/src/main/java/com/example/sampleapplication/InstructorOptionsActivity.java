c4b99ce7891f15e240735543500e6479cf272fe7
Navya Sri Alaparthi <alaparthins@outlook.com>
2022-10-22 21:26:48 -0500

    updated instructor enrolled

--- app/src/main/java/com/example/sampleapplication/InstructorOptionsActivity.java
+++ app/src/main/java/com/example/sampleapplication/InstructorOptionsActivity.java
@@ -15,5 +15,42 @@ public class InstructorOptionsActivity extends AppCompatActivity {
     ConstraintLayout id_scanQRCode,id_showAttendence;
     TextView id_selectedCourse;
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instructor_options);
+
+        id_showAttendence = findViewById(R.id.id_showAttendence);
+        id_scanQRCode = findViewById(R.id.id_scanQRCode);
+        id_selectedCourse = findViewById(R.id.id_selectedCourse);
+
+        try {
+            String course=getIntent().getStringExtra("CourseType");
+            id_selectedCourse.setText(course);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+        id_scanQRCode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(InstructorOptionsActivity.this,QRGeneratorActivity.class);
+                intent.putExtra("QRCODE",id_selectedCourse.getText().toString());
+                startActivity(intent);
+
+            }
+        });
+
+        id_showAttendence.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(InstructorOptionsActivity.this,AllStudentsAttendence.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
 
     }

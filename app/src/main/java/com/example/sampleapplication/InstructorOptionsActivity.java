7708579c933f1ea5048679c0c835395c5133b384
Navya Sri Alaparthi <alaparthins@outlook.com>
2022-10-22 21:20:50 -0500

    create class instructor options

--- app/src/main/java/com/example/sampleapplication/InstructorOptionsActivity.java
+++ app/src/main/java/com/example/sampleapplication/InstructorOptionsActivity.java
@@ -9,3 +9,11 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+public class InstructorOptionsActivity extends AppCompatActivity {
+
+
+    ConstraintLayout id_scanQRCode,id_showAttendence;
+    TextView id_selectedCourse;
+
+
+    }

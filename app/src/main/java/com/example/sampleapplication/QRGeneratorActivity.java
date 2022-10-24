e8149b7d256de5f9e91f1ee726944610ca7503d3
Manoj Kota <kota.manoj8@gmail.com>
2022-10-23 23:40:54 -0500

    Created QR generated Activity

---
+++ app/src/main/java/com/example/sampleapplication/QRGeneratorActivity.java
@@ -0,0 +1,35 @@
+package com.example.sampleapplication;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.Path;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+import java.nio.file.FileSystems;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
+public class QRGeneratorActivity extends AppCompatActivity {
+
+    

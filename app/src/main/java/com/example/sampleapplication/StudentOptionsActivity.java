e4d9270685a2ae127d72e4b0a171c6b6ad0da1a6
Kalla <S545139@nwmissouri.edu>
2022-10-23 20:38:21 -0500

    imported libraries

--- app/src/main/java/com/example/sampleapplication/StudentOptionsActivity.java
+++ app/src/main/java/com/example/sampleapplication/StudentOptionsActivity.java
@@ -1,2 +1,15 @@
 package com.example.sampleapplication;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import com.example.sampleapplication.student.StudentHomePage;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
